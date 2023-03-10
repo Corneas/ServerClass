421da0dc3b35e6d915c70ef0606be11ca0729587
Minkyu Kwak <lanilani7661@gmail.com>
2023-03-10 12:43:15 +0900

    2023.03.10 서버수업 / 클라이언트와 서버 연결

---
+++ Socket/Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("172.31.1.74"), 20000);
+                clientSocket.Connect(endPoint);
+
+                while(true)
+                {
+                    string str = Console.ReadLine();
+                    if (str == "exit")
+                    {
+                        break;
+                        //return;
+                    }
+                    // 직렬화
+                    byte[] buffer = Encoding.UTF8.GetBytes(str);
+                    clientSocket.Send(buffer);
+
+                    byte[] buffer2 = new byte[256];
+                    int bytesRead = clientSocket.Receive(buffer2);
+                    if(bytesRead < 1)
+                    {
+                        Console.WriteLine("서버의 연결 종료");
+                        return;
+                    }
+
+                    string str2 = Encoding.UTF8.GetString(buffer2);
+                    Console.WriteLine("받음 : " + str2);
+                }
+            }
+        }
+    }
+}
---
+++ Socket/Server/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("172.31.1.74"), 20000);
+
+                serverSocket.Bind(endPoint);
+
+                serverSocket.Listen(20);
+
+                using (Socket clientSocket = serverSocket.Accept())
+                {
+                    Console.WriteLine(clientSocket.RemoteEndPoint);
+                        
+                    while (true)
+                    {
+                        byte[] buffer = new byte[256];
+                        int totalByte = clientSocket.Receive(buffer);
+
+                        if (totalByte < 1)
+                        {
+                            Console.WriteLine("클라이언트의 연결종료");
+                            return;
+                        }
+                        // 역직렬화
+                        string str = Encoding.UTF8.GetString(buffer);
+                        Console.WriteLine(str);
+
+                        clientSocket.Send(buffer);
+                    }
+                }
+            }
+
+            //Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("172.31.1.74"), 20000);
+
+            //serverSocket.Bind(endPoint);
+
+            //// 연결 요청 대기방 (20개)
+            //serverSocket.Listen(20);
+
+            //// 연결 요청 수락
+            //Socket clientSocket = serverSocket.Accept();
+
+            //Console.WriteLine("연결됨 : " + clientSocket.RemoteEndPoint);
+        }
+    }
+}
