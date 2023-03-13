93868c143abf83a508b0f0527b0dc0070a40542c
Minkyu Kwak <lanilani7661@gmail.com>
2023-03-13 11:52:47 +0900

    쓰레드 기초, 동기/비동기

--- Socket/Client/Program.cs
+++ Socket/Client/Program.cs
@@ -2,43 +2,46 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
 
-            using (Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
-            {
+            /*using ())*/
+            //{
+                Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("172.31.1.74"), 20000);
-                clientSocket.Connect(endPoint);
+
+                await clientSocket.ConnectAsync(endPoint);
 
                 while(true)
                 {
                     string str = Console.ReadLine();
-                    if (str == "exit")
-                    {
-                        break;
-                        //return;
-                    }
-                    // 직렬화
+                    //if (str == "exit")
+                    //{
+                    //    break;
+                    //    //return;
+                    //}
+                    //// 직렬화
                     byte[] buffer = Encoding.UTF8.GetBytes(str);
-                    clientSocket.Send(buffer);
+                    await clientSocket.SendAsync(buffer, SocketFlags.None);
 
-                    byte[] buffer2 = new byte[256];
-                    int bytesRead = clientSocket.Receive(buffer2);
-                    if(bytesRead < 1)
-                    {
-                        Console.WriteLine("서버의 연결 종료");
-                        return;
-                    }
+                    //byte[] buffer2 = new byte[256];
+                    //int bytesRead = clientSocket.Receive(buffer2);
+                    //if(bytesRead < 1)
+                    //{
+                    //    Console.WriteLine("서버의 연결 종료");
+                    //    return;
+                    //}
 
-                    string str2 = Encoding.UTF8.GetString(buffer2);
-                    Console.WriteLine("받음 : " + str2);
+                    //string str2 = Encoding.UTF8.GetString(buffer2);
+                    //Console.WriteLine("받음 : " + str2);
                 }
-            }
+            //}
         }
     }
 }
--- Socket/Server/Program.cs
+++ Socket/Server/Program.cs
@@ -2,41 +2,53 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Server
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             using(Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
             {
                 IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("172.31.1.74"), 20000);
 
                 serverSocket.Bind(endPoint);
-
+                
                 serverSocket.Listen(20);
 
-                using (Socket clientSocket = serverSocket.Accept())
+                while (true)
                 {
-                    Console.WriteLine(clientSocket.RemoteEndPoint);
-                        
-                    while (true)
-                    {
-                        byte[] buffer = new byte[256];
-                        int totalByte = clientSocket.Receive(buffer);
+                    //using (Socket clientSocket = serverSocket.Accept())
+                    //{
+                        Socket clientSocket = await serverSocket.AcceptAsync();
+                        Console.WriteLine(clientSocket.RemoteEndPoint);
+                        ThreadPool.QueueUserWorkItem(ReadAsync, clientSocket);
 
-                        if (totalByte < 1)
-                        {
-                            Console.WriteLine("클라이언트의 연결종료");
-                            return;
-                        }
-                        // 역직렬화
-                        string str = Encoding.UTF8.GetString(buffer);
-                        Console.WriteLine(str);
+                        //Thread t1 = new Thread(() =>
+                        //{
+                        //    while (true)
+                        //    {
+                        //        byte[] buffer = new byte[256];
+                        //        int totalByte = clientSocket.Receive(buffer);
 
-                        clientSocket.Send(buffer);
-                    }
+                        //        if (totalByte < 1)
+                        //        {
+                        //            Console.WriteLine("클라이언트의 연결종료");
+                        //            clientSocket.Dispose();
+                        //            return;
+                        //        }
+                        //        // 역직렬화
+                        //        string str = Encoding.UTF8.GetString(buffer);
+                        //        Console.WriteLine(str);
+
+                        //        clientSocket.Send(buffer);
+                        //    }
+                        //});
+                        //t1.Start();
+                    //}
                 }
             }
 
@@ -53,5 +65,23 @@ namespace Server
 
             //Console.WriteLine("연결됨 : " + clientSocket.RemoteEndPoint);
         }
+
+        private static async void ReadAsync(object? sender)
+        {
+            Socket clientSocket = (Socket)sender;
+            while (true)
+            {
+                byte[] buffer = new byte[256];
+                int n1 = await clientSocket.ReceiveAsync(buffer, SocketFlags.None);
+                if(n1 < 1)
+                {
+                    Console.WriteLine("client disconnect");
+                    clientSocket.Dispose();
+                    return;
+                }
+                Console.WriteLine(Encoding.UTF8.GetString(buffer));
+            }
+        }
     }
+
 }
