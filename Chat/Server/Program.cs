e267da6a2163e19183d461eab53b08016e978983
Minkyu Kwak <lanilani7661@gmail.com>
2023-03-17 11:34:26 +0900

    배포해주신 채팅 프로그램 추가 ( 닷넷 버전이 안돼 실행불가 )

---
+++ Chat/Server/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Server;
+
+internal class Program
+{
+    const string ip = "192.168.200.120";
+
+    static async Task Main(string[] args)
+    {
+        Server server = new Server(ip, 20000, 10);
+        await server.StartAsync();
+    }
+}
