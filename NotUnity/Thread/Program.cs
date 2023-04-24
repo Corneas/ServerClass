a2d4a8fd683871f3a688cfaf09bb1d1a9ca223c7
Minkyu Kwak <lanilani7661@gmail.com>
2023-04-24 09:39:36 +0900

    파일 분할중

---
+++ NotUnity/Thread/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest2
+{
+    class Program
+    {
+
+        #region 1교시
+        /* static void MainThread(object state)
+        {
+            Console.WriteLine("Start MainThread");
+
+            while (_stop == false)
+            {
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string s;
+
+            ThreadPool.SetMaxThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+
+            for (int i = 0; i < 4; ++i)
+            {
+                // ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+                Task t = new Task(() => {while (true) { } }, TaskCreationOptions.LongRunning);
+                t.Start();
+            }
+
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            while (true)
+            {
+                s = Console.ReadLine();
+                if(s == "exit" || s == "Exit")
+                {
+                    Console.WriteLine("Exit this loop");
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        */
+        #endregion
+
+        volatile static bool _stop = false;
+
+        static void MainThread()
+        {
+            Console.WriteLine("Start MainThread");
+
+            while(_stop == false)
+            {
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            #region 2교시
+
+            Task t = new Task(MainThread);
+            t.Start();
+
+            Thread.Sleep(1000);
+
+            _stop = true;
+
+            Console.WriteLine("stop 호출");
+            Console.WriteLine("종료 대기중");
+            t.Wait();
+            Console.WriteLine("종료 성공");
+
+
+            #endregion
+
+        }
+    }
+}
---
+++ NotUnity/ThreadTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            object obj = new object();
+
+            Thread t1 = new Thread(() =>
+            {
+                for (int i = 0; i < 1000000; ++i)
+                {
+                    // 모니터
+                    //try
+                    //{
+                    //    Monitor.Enter(obj);
+                    //    num++;
+                    //}
+                    //finally
+                    //{
+                    //    Monitor.Exit(obj);
+                    //}
+
+                    // 간단하게 표현
+                    lock (obj)
+                    {
+                        num++;
+                    }
+                }
+            });
+            t1.Start();
+
+            Thread t2 = new Thread(() =>
+            {
+                for (int i = 0; i < 1000000; ++i)
+                {
+                    try
+                    {
+                        Monitor.Enter(obj);
+                        num++;
+                    }
+                    finally
+                    {
+                        Monitor.Exit(obj);
+                    }
+                }
+            });
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+            Console.WriteLine(num);
+
+        }
+    }
+}
