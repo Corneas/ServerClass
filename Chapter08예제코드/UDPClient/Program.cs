a62d09c39c14f63a25ca005b26bee96a385179a7
Minkyu Kwak <lanilani7661@gmail.com>
2023-08-25 13:53:05 +0900

    UDP 서버 테스트

---
+++ Chapter08예제코드/UDPClient/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static string SERVERIP = "255.255.255.255";
+        const int SERVERPORT = 9000;
+        const int BUFSIZE = 512;
+
+        static void Main(string[] args)
+        {
+            int retval;
+
+            Socket socket = null;
+
+            try
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(1);
+            }
+
+            IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse(SERVERIP), SERVERPORT);
+            byte[] buf = new byte[BUFSIZE];
+
+            while(true)
+            {
+                Console.Write("\n[보낼 데이터] ");
+                string data = Console.ReadLine();
+                if (data.Length == 0) break;
+
+                try
+                {
+                    // 데이터 send
+                    byte[] sendData = Encoding.Default.GetBytes(data);
+                    int size = sendData.Length;
+                    if (size > BUFSIZE) size = BUFSIZE;
+                    retval = socket.SendTo(sendData, 0, size, SocketFlags.None, serverAddress);
+                    Console.WriteLine("[UDP 클라이언트] {0} 바이트 전송", retval);
+
+                    //IPEndPoint anyAddress = new IPEndPoint(IPAddress.Any, 0);
+                    //EndPoint peerAddress = (EndPoint)anyAddress;
+                    //retval = socket.ReceiveFrom(buf, BUFSIZE, SocketFlags.None, ref peerAddress);
+
+                    //Console.WriteLine("[UDP 클라이언트] {0} 바이트 수신", retval);
+                    //Console.WriteLine("[받은 데이터] {0}", Encoding.Default.GetString(buf, 0, retval));
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    break;
+                }
+            }
+
+            socket.Close();
+
+        }
+    }
+}
---
+++ Chapter08예제코드/UDPServer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPServer
+{
+    class Program
+    {
+        const int SERVERPORT = 9000;
+        const int BUFSIZE = 512;
+
+        static void Main(string[] args)
+        {
+            int retval;
+
+            Socket socket = null;
+
+            try
+            {
+                // 소켓 생성
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                // 포트 설정
+                socket.Bind(new IPEndPoint(IPAddress.Any, SERVERPORT));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(1);
+            }
+
+            byte[] buf = new byte[BUFSIZE];
+
+            while (true)
+            {
+                try
+                {
+                    IPEndPoint anyaddress = new IPEndPoint(IPAddress.Any, 0);
+                    EndPoint peeraddress = (EndPoint)anyaddress;
+                    retval = socket.ReceiveFrom(buf, BUFSIZE, SocketFlags.None, ref peeraddress);
+
+                    Console.WriteLine("[UDP/{0}:{1}] {2}", 
+                        ((IPEndPoint)peeraddress).Address, 
+                        ((IPEndPoint)peeraddress).Port,
+                        Encoding.Default.GetString(buf,0,retval));
+
+                    // 데이터 송신
+                    // socket.SendTo(buf, 0, retval, SocketFlags.None, peeraddress);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    break;
+                }
+            }
+
+            socket.Close();
+        }
+    }
+}
